dc25967643c17caf8abd24d6260b7ad3e18083cc
TheSumSt_wincs <2240765672@qq.com>
2025-03-17 06:33:24 +0000

    week5: update LINUX assembly code

--- 5_Lab/Week5/IntArray_LINUX.s
+++ 5_Lab/Week5/IntArray_LINUX.s
@@ -10,21 +10,27 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$1, -48(%rbp)
-	movl	$2, -44(%rbp)
-	movl	$3, -40(%rbp)
-	movl	$4, -36(%rbp)
-	movl	$5, -32(%rbp)
+	pxor	%xmm0, %xmm0
+	movaps	%xmm0, -48(%rbp)
+	movd	%xmm0, -32(%rbp)
 	leaq	-48(%rbp), %rax
 	movq	%rax, -8(%rbp)
-	movl	-36(%rbp), %eax
+	movl	$1, -44(%rbp)
+	movl	-44(%rbp), %eax
 	movl	%eax, -12(%rbp)
 	movl	$1, -16(%rbp)
 	movl	-16(%rbp), %eax
-	addl	$2, %eax
+	addl	$1, %eax
+	cltq
+	movl	$2, -48(%rbp,%rax,4)
+	movl	-16(%rbp), %eax
+	addl	$1, %eax
 	cltq
 	movl	-48(%rbp,%rax,4), %eax
 	movl	%eax, -20(%rbp)
+	movq	-8(%rbp), %rax
+	addq	$12, %rax
+	movl	$3, (%rax)
 	movq	-8(%rbp), %rax
 	movl	12(%rax), %eax
 	movl	%eax, -24(%rbp)
