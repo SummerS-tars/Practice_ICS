731d412100605b7ff4b56220e8d88212a48f7c9a
TheSumSt <2240765672@qq.com>
2025-04-21 23:25:08 +0800

    note: update some test

--- 4_TestCode/10_Week10/ArrayReferencingBug/ReferencingBug.c
+++ 4_TestCode/10_Week10/ArrayReferencingBug/ReferencingBug.c
@@ -14,17 +14,36 @@ double fun(int i) {
 
 int main() {
     double d0 = fun(0) ;
-    printf("d0 = %f\n", d0) ;
+    printf("d0 = %lf\n", d0) ;
     double d1 = fun(1) ;
-    printf("d1 = %f\n", d1) ;
+    printf("d1 = %lf\n", d1) ;
     double d2 = fun(2) ;
-    printf("d2 = %f\n", d2) ;
+    printf("d2 = %lf\n", d2) ;
     double d3 = fun(3) ;
-    printf("d3 = %f\n", d3) ;
+    printf("d3 = %lf\n", d3) ;
     double d4 = fun(4) ;
-    printf("d4 = %f\n", d4) ;
+    printf("d4 = %lf\n", d4) ;
     double d6 = fun(5) ;
-    printf("d6 = %f\n", d6) ;
+    printf("d6 = %lf\n", d6) ;
     
     return 0;
 }
+
+/**
+ * Linux(x86-64) output:
+ * d0 = 3.140000
+ * d1 = 3.140000
+ * d2 = 3.140000
+ * d3 = 2.000001
+ * Segmentation fault (core dumped)
+ */
+
+ /**
+  * MacOS(arm64) output:
+  * d0 = 3.140000
+ * d1 = 3.140000
+ * d2 = 3.140000
+ * d3 = 2.000001
+ * d4 = 3.140000
+ * d6 = 3.140000
+  */
